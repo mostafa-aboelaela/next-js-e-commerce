ffb4eddf695e5a5acea707b84f4fd2b5ca1bcc87
mostafa-aboelaela <116121476+mostafa-aboelaela@users.noreply.github.com>
2023-05-21 21:26:35 +0300

    new

--- app/cancel/page.js
+++ app/cancel/page.js
@@ -1,10 +1,10 @@
-import Link from "next/link"
+import Link from "next/link";
 
 export default function CancelPage() {
-    return (
-        <div>
-            Gutted
-            <Link>Back home</Link>
-        </div>
-    )
+  return (
+    <div>
+      Gutted
+      <Link>Back home</Link>
+    </div>
+  );
 }
