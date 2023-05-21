6d9f035e0016b7775e933767670356b753410899
mostafa-aboelaela <116121476+mostafa-aboelaela@users.noreply.github.com>
2023-05-21 20:37:26 +0300

    lastchanges

--- app/Header.js
+++ app/Header.js
@@ -15,7 +15,7 @@ export default function Header() {
                 <Modal />
             )}
             <Link href={'/'}>
-                <h1 className='uppercase cursor-pointer hover:scale-110'>Fruit Shop</h1>
+                <h1 className='uppercase cursor-pointer hover:scale-110'>Fackahany Shop</h1>
             </Link>
             <div onClick={setOpenModal} className='relative cursor-pointer group grid place-items-center'>
                 {cartItems.length > 0 && (
--- app/product/page.js
+++ app/product/page.js
@@ -1,4 +1,5 @@
 "use client"
+import Image from "next/image"
 import useCart from "../(store)/store"
 
 export default function ProductPage(props) {
@@ -30,7 +31,7 @@ export default function ProductPage(props) {
         <div className="flex flex-col p-4">
             <div className="grid grid-cols-1 md:grid-cols-2 w-full max-w-[1000px] mx-auto">
                 <div className="md:p-2 md:shadow">
-                    <img src={productInfo.images[0]} alt={name} className="w-full h-full object-cover" />
+                    <Image src={productInfo.images[0]} alt={name} className="w-full h-full object-cover" />
                 </div>
                 <div className="flex flex-col gap-2 p-4">
                     <div className="flex md:flex-col md:items-start text-xl  items-center justify-between gap-2">
